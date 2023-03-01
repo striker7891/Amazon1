a485863ccd5a11e9cccd3fa4b5ddd7af9080a5ac
VinayakG777 <vinayakghumare08@gmail.com>
2023-03-01 20:42:21 +0530

    Sign in
    Sign in Successfully

---
+++ AmazonQaProject/src/com/qa/pageLayer/LogIn.java
@@ -0,0 +1,9 @@
+package com.qa.pageLayer;
+
+public class LogIn {
+	public static void main(String args[])
+	{
+		System.out.println(" log  in sucessfully");
+	}
+
+}
---
+++ AmazonQaProject/src/com/qa/pageLayer/SignIn.java
@@ -0,0 +1,9 @@
+package com.qa.pageLayer;
+
+public class SignIn {
+	public static void main(String args[])
+	{
+		System.out.println(" sign insucessfully");
+	}
+
+}
